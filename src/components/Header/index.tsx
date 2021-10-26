dac2ff5d89e803866347b110d3c23617e5c3c694
LarissaAllves <larissalvesf2@gmail.com>
2021-10-26 20:41:53 -0300

    incluindo modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+//criar props para receber a função que está no App
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="dt money" />
-        <button type="button">Nova Transação</button>
+        <img src={logoImg} alt="dt-money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
